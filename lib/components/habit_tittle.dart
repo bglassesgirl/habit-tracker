f32cf038f70544411cbba3c2af51c54ccf098754
Luana Braga <luanabragagomes9@gmail.com>
2024-01-29 09:37:56 -0300

    feat: habit ui #2

---
+++ lib/components/habit_tittle.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class HabitTittle extends StatelessWidget {
+  final bool isCompleted;
+  final String text;
+  final void Function(bool?)? onChanged;
+  final void Function(BuildContext)? editHabit;
+  final void Function(BuildContext)? deleteHabit;
+
+  const HabitTittle({
+    super.key,
+    required this.isCompleted,
+    required this.text,
+    required this.onChanged,
+    required this.editHabit,
+    required this.deleteHabit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: editHabit,
+              backgroundColor: Colors.grey.shade800,
+              icon: Icons.settings,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            SlidableAction(
+              onPressed: deleteHabit,
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ],
+        ),
+        child: GestureDetector(
+          onTap: () {
+            if (onChanged != null) {
+              onChanged!(!isCompleted);
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: isCompleted
+                  ? Colors.indigo
+                  : Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(12),
+            child: ListTile(
+              title: Text(
+                text,
+                style: TextStyle(
+                    color: isCompleted
+                        ? Colors.white
+                        : Theme.of(context).colorScheme.inversePrimary
+                      ),
+                  ),
+              // checkbox
+              leading: Checkbox(
+                activeColor: Colors.green[300],
+                value: isCompleted,
+                onChanged: onChanged,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/components/drawer.dart';
+import 'package:habit_tracker/components/habit_tittle.dart';
 import 'package:habit_tracker/detabase/habit_detabase.dart';
 import 'package:habit_tracker/models/habits.dart';
 import 'package:provider/provider.dart';
@@ -20,6 +21,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   final TextEditingController textController = TextEditingController();
+
   // create habits
   void createNewHabits() {
     showDialog(
@@ -28,30 +30,98 @@ class _HomePageState extends State<HomePage> {
         content: TextField(
           controller: textController,
           decoration: const InputDecoration(
-            hintText:"Adicione um novo hábito",
+            hintText: "Adicione um novo hábito",
           ),
         ),
         actions: [
           // save button
           // cria um botao e quando pressionado ele salva no banco e volta pra tela anterior
-          MaterialButton(onPressed: (){
-            // get the habit named
-            String habitName = textController.text;
-            context.read<HabitDatabase>().addHabit(habitName);
-            Navigator.pop(context);
-            textController.clear();
-          },
-          child: const Text('Salvar'),
+          MaterialButton(
+            onPressed: () {
+              // get the habit named
+              String habitName = textController.text;
+              context.read<HabitDatabase>().addHabit(habitName);
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Salvar'),
           ),
 
           // cancel button
           // it creates a button - on pressed he goes back (POP) to the latest screm
           // cria um buttao e ao ser pressionado ele apaga e volta pra tela anterior
-          MaterialButton(onPressed: (){
-            Navigator.pop(context);
-            textController.clear();
-          },
-          child: const Text("Cancelar"),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text("Cancelar"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // check habit, ON/OFF
+  void checkValueOnOff(bool? value, Habit habit) {
+    if (value != null) {
+      context.read<HabitDatabase>().updateHabitCompletion(habit.id, value);
+    }
+  }
+
+  // edit habit
+  void editHabitBox(Habit habit) {
+    textController.text = habit.name;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(controller: textController),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              String habitName = textController.text;
+              context
+                  .read<HabitDatabase>()
+                  .updateHabitName(habit.id, habitName);
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Salvar'),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text("Cancelar"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // delete habit
+  void deleteHabitBox(Habit habit) {
+    textController.text = habit.name;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Vocé tem certeza que deseja deletar?'),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              context
+                  .read<HabitDatabase>()
+                  .deleteHabit(habit.id);
+              Navigator.pop(context);
+            },
+            child: const Text('Delete'),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Cancelar"),
           ),
         ],
       ),
@@ -63,7 +133,10 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
-        title: const Text(''),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+
       ),
       drawer: const MyDrawer(),
       floatingActionButton: FloatingActionButton(
@@ -76,23 +149,27 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-   Widget _buildHabitsList(){
-      // habit db
-      final habitDatabase = context.watch<HabitDatabase>();
-      // current
-      List<Habit> currentHabits = habitDatabase.currentHabits;
-      return ListView.builder(
-        itemCount: currentHabits.length,
-        itemBuilder: (context, index){
-            // get each habit
-            final habit = currentHabits[index];
-            // check if the habit is completed td
-            bool isCompletedToday = isHabitCompletedToday(habit.completeDays);
-            // return habit tittle
-            return ListTile(
-              title:Text(habit.name),
-            );
-        },
-      );
-   }
+  Widget _buildHabitsList() {
+    // habit db
+    final habitDatabase = context.watch<HabitDatabase>();
+    // current
+    List<Habit> currentHabits = habitDatabase.currentHabits;
+    return ListView.builder(
+      itemCount: currentHabits.length,
+      itemBuilder: (context, index) {
+        // get each habit
+        final habit = currentHabits[index];
+        // check if the habit is completed td
+        bool isCompletedToday = isHabitCompletedToday(habit.completeDays);
+        // return habit tittle
+        return HabitTittle(
+          isCompleted: isCompletedToday,
+          text: habit.name,
+          onChanged: (value) => checkValueOnOff(value, habit),
+          editHabit: (context) => editHabitBox(habit),
+          deleteHabit: (context) => deleteHabitBox(habit),
+        );
+      },
+    );
+  }
 }
